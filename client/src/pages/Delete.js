588b487e52f1be8403bbaaeff41154233bb6e884
sreez1 <shreejal.mainali@deerwalk.edu.np>
2023-08-07 10:24:12 +0545

    cleanup

--- client/src/pages/Delete.js
+++ client/src/pages/Delete.js
@@ -1,7 +1,29 @@
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import {useState} from 'react';
-import {useParams} from 'react-router-dom';
 
-const Delete =()=>{
-    const {habitId} = useParams();
-}
+const Delete = ({ habitId, onDelete }) => {
+  const [loading, setLoading] = useState(false);
+
+  const handleDelete = async () => {
+    setLoading(true);
+    try {
+      await axios.delete(`http://localhost:3001/api/habit/${habitId}`);
+      // Call the onDelete function provided by the parent component to update the state after successful deletion
+      onDelete(habitId);
+    } catch (error) {
+      console.error('Error deleting habit:', error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div>
+      <button onClick={handleDelete} disabled={loading}>
+        {loading ? 'Deleting...' : 'Delete'}
+      </button>
+    </div>
+  );
+};
+
+export default Delete;
--- client/src/pages/Habit.js
+++ client/src/pages/Habit.js
@@ -2,6 +2,7 @@ import React from "react";
 import "react-datepicker/dist/react-datepicker.css";
 import { Outlet, Link } from "react-router-dom";
 import DateCalendarServerRequest from "./Calendar";
+import Delete from "./Delete"; 
 
 const Habit = ({ habit }) => {
   return (
@@ -12,6 +13,9 @@ const Habit = ({ habit }) => {
       <DateCalendarServerRequest startDate={habit.startDate} />
 
       <Link to={`/dashboard/edit/${habit._id}`}>Edit</Link>
+
+      <Delete habitId={habit._id} />
+
       <Outlet />
     </div>
   );
